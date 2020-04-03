8df0ac5d4b2b27552e33a0b1f932ea90d9adc2fe
k1 <keyvanramezan@gmail.com>
2020-04-03 14:17:15 +0430

    Basic Function Traning

---
+++ src/basicFunction.kt
@@ -0,0 +1,42 @@
+
+/*fun getGreeting(): String{
+    return "Hello Kotlin"
+}
+fun sayHello(): Unit{
+    println(getGreeting())
+
+}
+fun main() {
+    println("Hello World")
+    println(getGreeting())
+*/
+
+/*
+fun getGreeting(): String = "Hello Kotlin"
+fun sayHello(){
+    println(getGreeting())
+
+}
+fun main() {
+    println("Hello World")
+    println(getGreeting())
+    sayHello()
+}
+*/
+
+/*fun sayHello(itemToGreet:String){
+    val msg = "Hello " + itemToGreet
+    println(msg)
+}
+
+ */
+/*fun sayHello(itemToGreet:String){
+    val msg = "Hello $itemToGreet"
+    println(msg)
+}*/
+fun sayHello(greeting:String, itemToGreet: String) = println("$greeting $itemToGreet")
+fun main() {
+    sayHello("Hey","Kotlin")
+    sayHello("Hello","World")
+
+}
