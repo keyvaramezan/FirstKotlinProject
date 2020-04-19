092f36a2a85399f73ec36a13f5b2d1ad10b9b7d6
k.arab <keyvanramezan@gmail.com>
2020-04-19 12:35:02 +0430

    this is a kotlin trainig in office

--- src/EntityFactory(ObjectDecleration).kt
+++ src/EntityFactory(ObjectDecleration).kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 //lesson 1
 /*
 object EntityFactory{
@@ -14,6 +16,7 @@ fun main() {
 }*/
 
 //lesson 2
+/*
 object EntityFactory{
     fun create() = Entity("id", "name")
 }
@@ -28,4 +31,44 @@ class Entity (val id: String, val name: String){
 fun main() {
     val entity = EntityFactory.create()
     println(entity)
+}*/
+
+
+//Enum Classes lesson 1
+
+enum class EntityType{
+    EASY, MEDIUM, HARD;
+
+    fun getFormattedName() = name.toLowerCase().capitalize()
 }
+
+object EntityFactory{
+    fun create(type: EntityType) : Entity{
+        val id = UUID.randomUUID().toString();
+        val name = when(type){
+            //EntityType.EASY -> "Easy"
+            EntityType.EASY -> type.name
+            //EntityType.MEDIUM -> "Medium"
+            EntityType.MEDIUM -> type.getFormattedName()
+            EntityType.HARD -> "Hard"
+        }
+        return Entity(id,name)
+    }
+}
+
+
+class Entity (val id: String, val name: String){
+    override fun toString(): String {
+        return "id: $id, name: $name"
+    }
+
+}
+
+fun main() {
+    val entity = EntityFactory.create(EntityType.EASY)
+    println(entity)
+    val mediumEnity = EntityFactory.create(EntityType.MEDIUM)
+    println(mediumEnity)
+}
+//
+
