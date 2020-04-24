cdd50bbb23943581a0d8c9a994b3fe14ce885a23
k1 <keyvanramezan@gmail.com>
2020-04-24 13:54:37 +0430

    train sealed class

--- src/EntityFactory(ObjectDecleration).kt
+++ src/EntityFactory(ObjectDecleration).kt
@@ -37,7 +37,7 @@ fun main() {
 //Enum Classes lesson 1
 
 enum class EntityType{
-    EASY, MEDIUM, HARD;
+    Help, EASY, MEDIUM, HARD;
 
     fun getFormattedName() = name.toLowerCase().capitalize()
 }
@@ -51,24 +51,59 @@ object EntityFactory{
             //EntityType.MEDIUM -> "Medium"
             EntityType.MEDIUM -> type.getFormattedName()
             EntityType.HARD -> "Hard"
+
+            // lesson1 for sealed Class
+            EntityType.Help -> type.getFormattedName()
         }
-        return Entity(id,name)
+        //return Entity(id,name)
+
+        //lesson 1 for Sealed Class
+        return when(type){
+            EntityType.EASY -> Entity.Easy(id, name)
+            EntityType.MEDIUM -> Entity.Medium(id, name)
+            EntityType.HARD -> Entity.Hard(id, name, 2f)
+            EntityType.Help -> Entity.Help
+        }
     }
 }
 
 
-class Entity (val id: String, val name: String){
+/*class Entity (val id: String, val name: String){
     override fun toString(): String {
         return "id: $id, name: $name"
     }
 
+}*/
+
+
+//lesson 1 for Sealed Class
+sealed class Entity (){
+    object Help : Entity() {
+        val name = "Help"
+    }
+    data class Easy(val id: String, val name: String) : Entity()
+    data class Medium(val id: String, val name: String) :Entity()
+    data class Hard(val id: String, val name: String, val multiplier: Float) : Entity()
 }
 
+
 fun main() {
-    val entity = EntityFactory.create(EntityType.EASY)
-    println(entity)
+    val entity: Entity = EntityFactory.create(EntityType.Help)
+
+    // lesson for enum class
+   /* println(entity)
     val mediumEnity = EntityFactory.create(EntityType.MEDIUM)
-    println(mediumEnity)
+    println(mediumEnity)*/
+
+    // lesson 1 for sealed class
+    val msg = when(entity){
+        Entity.Help -> "help class"
+        is Entity.Easy -> "easy class"
+        is Entity.Medium -> "medium class"
+        is Entity.Hard -> "hard class"
+    }
+
+    println(msg)
 }
 //
 
